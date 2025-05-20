62eab909b4d25f3694d2c745503a8884230b3822
atulj10 <atulanandj10@gmail.com>
2025-05-20 16:49:01 +0530

    Budget overload email sending functionality added along with templates and all

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import { inngest } from "@/app/lib/inngest/client";
+import { checkBudgetAlerts } from "@/app/lib/inngest/functions";
+import { serve } from "inngest/next";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    checkBudgetAlerts
+  ],
+});
